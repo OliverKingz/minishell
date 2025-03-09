fbf9f1fbe0ed4d87730dff73012702b11834318b
ozamora- <ozamora-@student.42madrid.com>
2025-03-09 02:03:07 +0100

    fix: leak when only enter, history. To do it I needed to use while(1)

--- src/shell.c
+++ src/shell.c
@@ -6,7 +6,7 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 18:27:41 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/03/09 01:17:55 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/03/09 02:02:43 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@ t_shell	*create_shell(char **env)
 
 	mini_sh = (t_shell *)ft_calloc(1, sizeof(t_shell));
 	if (!mini_sh)
-		my_perr("Failed to malloc shell", true, errno);
+		return (my_perr("Failed to malloc shell", false, errno), NULL);
 	mini_sh->env = init_envlist(env); //Mirar env de emergencia y PATH de emergencia
+	// Mirar que pasa cuando init devuelve NULL
 	mini_sh->last_exit_status = 0;
 	mini_sh->input = NULL;
 	return (mini_sh);
@@ -30,28 +31,27 @@ int	loop_shell(t_shell *mini_sh)
 	char	*read_line;
 	int		exit_status;
 
-	read_line = readline(PROMPT);
-	while (read_line)
+	while (1)
 	{
-		if (ft_strncmp(read_line, "", -1) != 0)
-			add_history(read_line); // No funciona si hago prints antes
+		read_line = readline(PROMPT);
+		if (!read_line)
+			break;
+		if (ft_strncmp(read_line, "", -1) == 0)
+			continue ;
+		else
+			add_history(read_line);
 		if (!validate_rline_syntax(read_line))
 		{
-			free(read_line);
+			free(read_line), read_line = NULL;
 			continue ;
 		}
 		mini_sh->input = init_input(mini_sh, read_line);
-		if (!mini_sh->input)
-			return (ft_puterr("init_input failed\n"), free_shell(mini_sh), EXIT_FAILURE);
-		//ft_printf("%s\n", mini_sh->input->read_line);
 		//print_tokenslist_short(mini_sh->input->token_lst);
 		//print_envlist(mini_sh->env);
 		(free(read_line), free_input(mini_sh), mini_sh->input = NULL);
-		read_line = readline(PROMPT);
 	}
 	exit_status = mini_sh->last_exit_status;
-	free_shell(mini_sh);
-	return (exit_status);
+	return (free_shell(mini_sh), exit_status);
 }
 
 void	free_shell(t_shell *mini_sh)
